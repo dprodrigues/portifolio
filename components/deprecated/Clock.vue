bc4c1eec42f8a4921e1d80664bbf021e00c9c862
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-01-14 00:53:07 -0300

    refactor: remove and move some vue files to deprecated

---
+++ components/deprecated/Clock.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ currentTime }}</p>
+  </div>
+</template>
+
+<script setup>
+import { getCurrentHour } from '~/utils/getCurrentHour'
+const currentTime = useState('currentTime', () => '')
+
+await callOnce(() => {
+  currentTime.value = getCurrentHour();
+
+  setInterval(() => {
+    currentTime.value = getCurrentHour();
+  }, 1000)
+})
+</script>
+
+<style scoped>
+p {
+  font-size: 0.875rem;
+}
+</style>
---
+++ components/deprecated/TimeAndDate.vue
@@ -0,0 +1,40 @@
+<script setup>
+import NuxtTime from 'nuxt-time'
+import Date from './Date.vue';
+
+const date = ref('2023-02-11T08:24:08.396Z')
+
+onMounted(() => {
+  date.value = Date.now()
+})
+
+// const changeDate = () => {
+//   date.value = Date.now()
+// }
+
+// export default {
+//   components: {
+//     Date,
+//     NuxtTime
+//   }
+// }
+</script>
+
+<template>
+  <div class="container">
+    <Date />
+
+    <NuxtTime :datetime="date" second="numeric" month="long" day="numeric" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  align-items: flex-end;
+  display: flex;
+}
+
+p {
+  font-size: 0.875rem;
+}
+</style>
