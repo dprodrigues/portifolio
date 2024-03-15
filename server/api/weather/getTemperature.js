e520dfbb96b938a5784f8e901d092267ff38346f
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-03-15 10:10:12 -0300

    refactor: delete old vue components

--- server/api/weather/getTemperature.js
+++
@@ -1,9 +0,0 @@
-import { getTemperature } from "~/server/src/api/weather";
-
-export default defineEventHandler(async () => {
-  try {
-    return await getTemperature();
-  } catch (error) {
-    return error;
-  }
-});
--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  WEATHER: {
-    URL: process.env.WEATHER_URL,
-    KEY: process.env.WEATHER_API_KEY,
-  },
-};
