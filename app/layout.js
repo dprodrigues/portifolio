06775d5ea05f65e27ec07ff528733347ca9efc47
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-03-19 20:56:33 -0300

    feat: add poppins font to layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
+import { Poppins } from 'next/font/google'
 import Header from '@/components/header'
 import { Main } from '@/styles/layout'
 import '@/assets/base.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+})
 
 export const metadata = {
   title: 'Daniel Rodrigues',
@@ -10,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         <Header />
 
         <Main>{children}</Main>
